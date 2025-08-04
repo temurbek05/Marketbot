26db5b66bc876311478184469850c5e58e7bf9a8
temurbek05 <114956432+temurbek05@users.noreply.github.com>
2025-08-04 10:36:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "📚 Ushbu bot Gulruh Markayeva tomonidan yozilgan kitoblar sotuviga mo‘ljallangan.\n\n"
         "👩‍💼 Muallif: Gulruh Markayeva – psixologik ruhdagi blog asoschisi, 5 yillik tajribaga ega ingliz tili o’qituvchisi va zamonaviy adabiyotga kirib kelayotgan istiqbolli muallif.\n\n"
         "📌 Telegram kanal: @yupiterlik\n\n"
-        "💰 Kitob narxi: har bir kitob tavsifida ko‘rsatilgan.\n\n"
+        "💰 Kitob narxi: Har bir kitob tavsifida ko‘rsatilgan.\n\n"
         "📞 Savollar bo'yicha admin: @jupiter_ads",
         reply_markup=keyboard
     )
@@ -297,5 +297,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
